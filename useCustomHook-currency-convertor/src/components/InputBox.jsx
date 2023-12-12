ff44634c633b852604665e23fba50674594349a5
Sushankznation <sushantbharwal@gmail.com>
2023-12-12 15:32:28 +0530

    added userid

--- useCustomHook-currency-convertor/src/components/InputBox.jsx
+++ useCustomHook-currency-convertor/src/components/InputBox.jsx
@@ -1,3 +1,4 @@
+import { useState, useId } from "react";
 function InputBox({
   label,
   amount,
@@ -9,15 +10,22 @@ function InputBox({
   currencyDisable = false,
   className = "",
 }) {
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+          className="text-black/40 mb-2 inline-block"
+          htmlFor={amountInputId}
+        >
+          {label}
+        </label>
         <input
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
           disabled={amountDisable}
+          id={amountInputId}
           value={amount}
           onChange={(e) =>
             onAmountChange && onAmountChange(Number(e.target.value))
@@ -33,7 +41,9 @@ function InputBox({
           disabled={currencyDisable}
         >
           {currencyOptions.map((currency, index) => (
-            <option key={index} value={currency}>{currency}</option>
+            <option key={index} value={currency}>
+              {currency}
+            </option>
           ))}
         </select>
       </div>
