f3128a25acc4b009cad6399992223a89a367fbf2
Sushankznation <sushantbharwal@gmail.com>
2023-12-12 15:36:52 +0530

    added new State

--- useCustomHook-currency-convertor/src/App.jsx
+++ useCustomHook-currency-convertor/src/App.jsx
@@ -1,8 +1,11 @@
+import { InputBox } from "./components";
+import useCurrencyInfo from "./customHook/useCurrencyInfo";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("inr");
+  const [to, setTo] = useState("usd");
+  const [convertedAmount, setConverted] = useState(0);
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
