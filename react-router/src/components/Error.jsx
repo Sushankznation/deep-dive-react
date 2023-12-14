9981d2bcba377ee8f499e6ceed4b802299b8bb36
Sushankznation <sushantbharwal@gmail.com>
2023-12-14 11:51:54 +0530

    added

--- react-router/src/components/Error.jsx
+++ react-router/src/components/Error.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function Error() {
   return (
-    <div className="flex justify-center items-center ">
+    <div className="flex justify-center place-items-center place-content-center ">
       <h1>Error No route Found Please Move to </h1> <br />
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         <Link to="/" className="hover:underline">
