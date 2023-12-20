d4265bc0e87d5b0dff28ee47be5bd2396b8f1995
Sushankznation <sushantbharwal@gmail.com>
2023-12-20 12:55:44 +0530

    Added Context.api

--- context-api-react/src/App.jsx
+++ context-api-react/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-transform: uppercase">Hello from tailwind css</h1>
+      <h1 className="text-transform: uppercase">Hello from Hello css</h1>
     </>
   );
 }
