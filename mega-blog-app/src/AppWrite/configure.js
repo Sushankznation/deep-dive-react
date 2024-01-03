8a8de9928ce6ae5743cd6e21b49eeb9eaa358875
Sushankznation <sushantbharwal@gmail.com>
2024-01-03 18:31:25 +0530

    added to configure

---
+++ mega-blog-app/src/AppWrite/configure.js
@@ -0,0 +1,35 @@
+import config from "../config/config";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(config.appWriteUrl)
+      .setProject(config.appWriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+  async createPost({ title, slug, content, featuredImg, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        config.appWriteDatabaseId,
+        config.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImg,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Errro ", error);
+    }
+  }
+}
+const service = new Service();
+export default service;
