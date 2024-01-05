b19799e075bee2608e827551cd3fe2f78c9f1e13
Sushankznation <sushantbharwal@gmail.com>
2024-01-05 17:30:13 +0530

    done

--- mega-blog-app/src/components/Header/Header.jsx
+++ mega-blog-app/src/components/Header/Header.jsx
@@ -41,18 +41,7 @@ export default function Header() {
             <Link to="/">LOGO</Link>
           </div>
           <ul className="flex ml-auto">
-            {navItems.map((item) =>
-              item.active ? (
-                <li key={item.name}>
-                  <button
-                    onClick={() => navigate(item.slug)}
-                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-400 rounded-full"
-                  >
-                    {item.name}
-                  </button>
-                </li>
-              ) : null
-            )}
+           
           </ul>
           {authStatus && (
             <li>
