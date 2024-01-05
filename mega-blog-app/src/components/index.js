ee5b049de4a997587b74cd5d44bec46ece8af3eb
Sushankznation <sushantbharwal@gmail.com>
2024-01-05 12:28:23 +0530

    added header

--- mega-blog-app/src/components/index.js
+++ mega-blog-app/src/components/index.js
@@ -1,4 +1,5 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './container/Container'
-export { Header, Footer ,Container};
+import Logout from './Header/Logout.jsx'
+export { Header, Footer ,Container,Logout};
