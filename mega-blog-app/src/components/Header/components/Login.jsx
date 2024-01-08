747607a4c5a3323ead576db68f63fb1c4dd1c6e0
Sushankznation <sushantbharwal@gmail.com>
2024-01-08 16:09:48 +0530

    added all functionality in  Login.jsx

--- mega-blog-app/src/components/Header/components/Login.jsx
+++ mega-blog-app/src/components/Header/components/Login.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login as authLogin } from "../../../Redux/features/authSlice";
-import { Button, Input } from "../../../UI";
+import { Button, Input } from "../../../UI/index";
 import AuthService from "../../../AppWrite/auth";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
@@ -36,19 +36,57 @@ export default function Login() {
     >
       <div className="mb-2 flex justify-center">
         <span className="inline-block w-full max-w-[100px]">LOGO</span>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign in to your account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          Don&apos;t have any account?&nbsp;
+          <Link
+            to="/signup"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign Up
+          </Link>
+        </p>
+        {error && (
+          <p className="mt-2 text-center text-base text-red-500">{error}</p>
+        )}
+        {/* handleSubmit is the fixed Keyword in react-hook-form visit: "https://react-hook-form.com/get-started" for more */}
+        <form onSubmit={handleSubmit(login)} className="mt-8">
+          <div className="space-x-5">
+            <Input
+              type="email"
+              name="email"
+              label="Email :"
+              placeholder="Enter you email"
+              // We need to spread this register every time because if we don't do this then it automatically overwrite the  previous value
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPattern: (value) =>
+                    value.includes(
+                      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)
+                    ) || "email address must be a valid Mail id ",
+                },
+              })}
+            />
+
+            <Input
+              type="password"
+              name="password"
+              label="Password :"
+              placeholder="Enter you password"
+              // We need to spread this register every time because if we don't do this then it automatically overwrite the  previous value
+              {...register("password", {
+                required: true,
+              })}
+            />
+            <Button type="submit" className="w-full">
+              Sign In
+            </Button>
+          </div>
+        </form>
       </div>
-      <h2 className="text-center text-2xl font-bold leading-tight">
-        Sign in to your account
-      </h2>
-      <p className="mt-2 text-center text-base text-black/60">
-        Don&apos;t have any account?&nbsp;
-        <Link
-          to="/signup"
-          className="font-medium text-primary transition-all duration-200 hover:underline"
-        >
-          Sign Up
-        </Link>
-      </p>
     </div>
   );
 }
