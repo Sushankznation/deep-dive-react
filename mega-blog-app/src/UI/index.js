2889cedfe71b1dd265dfd9b567a1743af9ddb74e
Sushankznation <sushantbharwal@gmail.com>
2024-01-08 15:45:13 +0530

    added login nad index.js here

---
+++ mega-blog-app/src/UI/index.js
@@ -0,0 +1,6 @@
+import Button from './Button'
+import Input from './Input'
+import Loader from './Loader'
+import PostCard from './PostCard'
+import Select from './Select'
+export {Button,Input,Loader,PostCard,Select}
