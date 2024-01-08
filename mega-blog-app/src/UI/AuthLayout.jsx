f520350366a633139435592ab27b5b04d847b836
Sushankznation <sushantbharwal@gmail.com>
2024-01-08 17:52:56 +0530

    updated the AuthLayout

---
+++ mega-blog-app/src/UI/AuthLayout.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    } else {
+      setLoader(false);
+    }
+  }, [authStatus, navigate, authentication]);
+  return <div></div>;
+}
