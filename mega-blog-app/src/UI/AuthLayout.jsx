79609d9fa500ec60de858616d5bc98a6458984b0
Sushankznation <sushantbharwal@gmail.com>
2024-01-09 11:10:34 +0530

    added comment in AuthLayout

--- mega-blog-app/src/UI/AuthLayout.jsx
+++ mega-blog-app/src/UI/AuthLayout.jsx
@@ -18,3 +18,25 @@ export default function Protected({ children, authentication = true }) {
   }, [authStatus, navigate, authentication]);
   return loader ? <Loader /> : children;
 }
+/*
+  The `Protected` component is designed to protect routes based on authentication status using React and Redux.
+  
+  Parameters:
+  - `children`: The content to be rendered if authentication is successful.
+  - `authentication`: A boolean flag indicating whether authentication is required (default: true).
+  
+  It utilizes React features such as `useEffect`, `useState`, and hooks like `useSelector` from "react-redux" for managing the component's behavior.
+
+  Inside the component:
+  - `navigate`: A function from "react-router-dom" for navigating to different routes.
+  - `loader`: State variable to control the rendering of a loading indicator.
+  - `authStatus`: Extracted from the Redux store using `useSelector`, representing the current authentication status.
+
+  The `useEffect` hook:
+  - Redirects to "/login" if authentication is required and the user is not authenticated.
+  - Redirects to "/" if authentication is not required and the user is authenticated.
+  - Sets the `loader` state to `false` once the authentication checks are complete.
+
+  The component returns either a `Loader` component while authentication status is being verified, or the `children` content if authentication is successful.
+
+  Example Usage:
