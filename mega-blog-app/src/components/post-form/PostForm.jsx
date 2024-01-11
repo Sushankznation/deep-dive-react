818a9761f3bc3764997a2edde0efff438e4df983
Sushankznation <sushantbharwal@gmail.com>
2024-01-11 14:58:41 +0530

    added taoster

--- mega-blog-app/src/components/post-form/PostForm.jsx
+++ mega-blog-app/src/components/post-form/PostForm.jsx
@@ -7,6 +7,8 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function PostForm({ post }) {
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
   const { register, handleSubmit, watch, setValue, control, getValues } =
     useForm({
       defaultValues: {
@@ -16,8 +18,26 @@ export default function PostForm({ post }) {
         status: post?.status || "active",
       },
     });
-  const navigate = useNavigate();
-  const userData = useSelector((state) => state.auth.userData);
 
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? appwriteService.uploadFile(data.image[0])
+        : null;
+      if (file) {
+        appwriteService.deleteFile(post.featuredImage);
+      }
+      const dbPost = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+        if(dbPost) {
+          navigate(`/post/${dbPost.$id}`);
+        }else{
+          const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null;
+        }
+     
+    }
+  };
   return <div></div>;
 }
--- mega-blog-app/src/main.jsx
+++ mega-blog-app/src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Toaster from "./UI/Toaster.jsx";
 import { Provider } from "react-redux";
 import store from "./Redux/store/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
+      <Toaster />
     </React.StrictMode>
   </Provider>
 );
